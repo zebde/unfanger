421368724af0e12be5525784c11db7a323e006c7
Chris Taylor <chris@taylr.ch>
2016-02-12 19:55:40 +0000

    Made code better, added tuples to handles multiple pattern/replacement pairs

--- python/unfanger.py
+++ python/unfanger.py
@@ -1,11 +1,22 @@
 import sys
+import re
 
 
-def doUnfang():
-    with open(sys.argv[1]) as f:
-        for line in f:
-            print(line.replace("[.]", "."))
+fangs = [
+    ('\[\.\]', '.'),
+    ('\[at\]', '@'),
+    ('hxxp:\/\/', 'http://'),
+    ('hxxps:\/\/', 'http://'),
+]
+
+
+def doUnfang(var):
+    for pattern, value in fangs:
+        var = re.sub(pattern, value, var)
+    return var
 
 
 if __name__ == '__main__':
-    doUnfang()
+    with open(sys.argv[1]) as f:
+        for line in f:
+            print(doUnfang(line.rstrip('\n')))
