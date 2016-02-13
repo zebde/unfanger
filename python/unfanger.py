7fc58cadbac307755ac17c4b5dad14f0da19ea0e
Chris Taylor <chris@taylr.ch>
2016-02-13 20:53:21 +0000

    Added docstring

--- python/unfanger.py
+++ python/unfanger.py
@@ -1,3 +1,11 @@
+"""
+Unfangs stuff.
+
+The script will look at the first variable passed to it and will attempt to
+open that as a file and perform various de-fanging on the file based on the
+FANGS list.
+"""
+
 import sys
 import re
 
